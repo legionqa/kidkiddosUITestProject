a30805bfbd8a437d176347e75f5518ae780d5704
Lev <lev.gorfel@centro.net>
2019-12-30 15:59:03 -0500

    Add Login test

--- src/main/java/Consts/Consts.java
+++ src/main/java/Consts/Consts.java
@@ -2,5 +2,6 @@ package Consts;
 
 public class Consts {
     public static final String MAIN_URL = "https://kidkiddos.com/";
+    public static final String MAIN_URL2 = "https://kidkiddos.com/";
 
 }
